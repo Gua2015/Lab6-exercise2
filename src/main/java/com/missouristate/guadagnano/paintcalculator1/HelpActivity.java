5e70a5acd6b27d368d701f1ed2932adda5da72ee
Gua2015 <55575273+Gua2015@users.noreply.github.com>
2019-11-07 22:37:47 -0600

    Add files via upload

---
+++ src/main/java/com/missouristate/guadagnano/paintcalculator1/HelpActivity.java
@@ -0,0 +1,18 @@
+package com.missouristate.guadagnano.paintcalculator1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HelpActivity extends Activity {
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_layout);
+    }
+
+    public void gotoInput(View view){
+        finish();
+    }
+}
+
